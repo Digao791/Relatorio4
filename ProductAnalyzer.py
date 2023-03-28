4ac56d1de3657394309053088b1c85a7a765ea6e
Rodrigo Fraga da Costa <rodrigo-fragacosta@hotmail.com>
2023-03-27 21:57:04 -0300

    Detalhando linhas do código

--- ProductAnalyzer.py
+++ ProductAnalyzer.py
@@ -1,14 +1,19 @@
 from database import Database
+
+
 db = Database(database="Mercado", collection="data")
 db.resetDatabase()
 data = db.collection.find()
 
+#Função PipeLine do Gasto total do Cliente B
 def TotalB():
     return db.collection.aggregate ([
         {"$unwind": "$produtos"},
         {"$match": {"cliente_id":"B"}},
         {"$group": {"_id": {"cliente": "$cliente_id"}, "total": {"$sum": {"$multiply": ["$produtos.quantidade", "$produtos.preco"]}}}},
 ])
+
+#Função PipeLine do produto menos vendido
 def ProdutoMenosVendido():
     return db.collection.aggregate([
     {"$unwind": "$produtos"},
@@ -16,6 +21,8 @@ def ProdutoMenosVendido():
     {"$sort": {"total_vendido": 1}},
     {"$limit": 1}
 ])
+
+#Função PipeLine do cliente que menos gastou com uma compra
 def ClienteQueMenosGastou():
     return db.collection.aggregate([
         {"$unwind": "$produtos"},
@@ -25,6 +32,8 @@ def ClienteQueMenosGastou():
         {"$group": {"_id": "$_id.data", "cliente": {"$first": "$_id.cliente"}, "total": {"$first": "$total"}}}
 
 ])
+
+#Função PipeLine dos produtos comprados mais de uma vez
 def ListaDosProdutos():
     return db.collection.aggregate([
     {"$unwind": "$produtos"},
