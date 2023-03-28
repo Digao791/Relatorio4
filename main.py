f8ec0c4ebc0cb2bf721838490a4cc6ca2ab5e473
Rodrigo Fraga da Costa <rodrigo-fragacosta@hotmail.com>
2023-03-27 21:52:29 -0300

    Detalhando linhas do código

--- main.py
+++ main.py
@@ -7,15 +7,17 @@ db = Database(database="Mercado", collection="mercado")
 db.resetDatabase()
 mercado = db.collection.find()
 
-result = TotalB()
-writeAJson(result, "Total_gasto_do_cliente_B")
+#Escrevendo os Arquivos JSON
 
-result1 = ProdutoMenosVendido()
-writeAJson(result1, "Produto_menos_vendido")
+#Arquivo do Total gasto do Cliente B
+writeAJson(TotalB(), "Total_gasto_do_cliente_B")
 
-result2 = ClienteQueMenosGastou()
-writeAJson(result2, "Cliente_que_menos_gastou_em_uma_unica_compra")
+#Arquivo do produto menos vendido
+writeAJson(ProdutoMenosVendido(), "Produto_menos_vendido")
 
-result3 = ListaDosProdutos()
-writeAJson(result3, "Produtos_que_tiveram_uma_quantidade_vendida_acima_de_2_unidades")
+#Arquivo do cliente que menos gastou realizando uma unica compra
+writeAJson(ClienteQueMenosGastou(), "Cliente_que_menos_gastou_em_uma_unica_compra")
+
+#Arquivo dos produtos vendidos mais de uma vez
+writeAJson(ListaDosProdutos(), "Produtos_que_tiveram_uma_quantidade_vendida_acima_de_2_unidades")
 
